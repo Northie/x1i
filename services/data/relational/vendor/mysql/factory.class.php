97d2df094b6a013bb75549f04be4b434b42acf12
Chris Rutledge <chris.rutledge@gmail.com>
2018-09-11 15:49:44 +0100

    sending the connection not the connector

--- services/data/relational/vendor/mysql/factory.class.php
+++ services/data/relational/vendor/mysql/factory.class.php
@@ -10,9 +10,16 @@ class factory {
             $id = json_encode($settings);
             $a = \settings\registry::Load()->get($id);
             if(!$a) {
+                //not ambiguous at all!!!
+                list($type,$settings['type']) = explode("/",$settings['type']); //$settings['type'] type will be something like 'relational/mysql' we needed all of it to get here, but only the last part to move forward!!!!
+                
                 $connector = new \services\data\relational\connector($settings); //relational / pdo uses a common connection mechanism
+                //manually connect, legacy, refactor into connector above?
+
+                $accessor = new \services\data\relational\accessor($connector->connect());
+                
                 //give the vendor specific adapter the pdo connection
-                $a = new adapter($connector);
+                $a = new adapter($accessor);
                 \settings\registry::Load()->set($id,$a);
             }
             return $a;
