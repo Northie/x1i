01ba54004dc524eb4abaf7748b7736b6b2f38b61
Chris Rutledge <chris.rutledge@gmail.com>
2018-09-27 09:08:43 +0100

    x1 folder tidyup and refactor of none cache

--- services/data/cache/vendor/none/adapter.class.php
+++ services/data/cache/vendor/none/adapter.class.php
@@ -5,30 +5,34 @@ namespace services\data\cache\vendor\none;
 class adapter extends \services\data\adapter {
 
 	public function __construct($settings) {
-
+		
 	}
 
-	public function create($key, $data,$lifetime=false) {
-			return true;
+	public function create($data, $id = false) {
+		return true;
 	}
 
 	public function read($key) {
-			return false;
+		return false;
 	}
 
-	public function update($key, $data,$lifetime=false) {
-			return $this->create($key, $data,$lifetime=false);
+	public function update($data, $conditions = false) {
+		return $this->create($key, $data);
 	}
 
-	
-	public function delete($key,$force=false) {
-			return true;
+	public function delete($key, $force = false) {
+		return true;
 	}
 
-		private function getLifetime() {
-			if(($cacheLifetime = \settings\general::Load()->get(['CACHE_LIFETIME']))) {
-				return $cacheLifetime;
-			}
-			return 3600;
+	private function getLifetime() {
+		if (($cacheLifetime = \settings\general::Load()->get(['CACHE_LIFETIME']))) {
+			return $cacheLifetime;
 		}
+		return 3600;
+	}
+	
+	public function query($query, $parameters = false) {
+		return [];
+	}
+
 }
